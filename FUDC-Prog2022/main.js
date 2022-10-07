fb9b9c4c3af8dc8874ac87b999861876f6f1c1e5
MiguelGomez <miguelgomez01101@gmail.com>
2022-10-07 17:22:54 -0500

    Programa actualizado 7/10/2022

---
+++ FUDC-Prog2022/main.js
@@ -0,0 +1,94 @@
+// Viernes 22
+var scrollViernes22 = document.getElementById("scrollViernes22");
+
+function izVie22() {
+  scrollViernes22.scrollBy({left: -300, behavior: "smooth"});
+
+}
+function derVie22() {
+  scrollViernes22.scrollBy({left: 300, behavior: "smooth"});
+}
+
+
+// Sábado 23
+var scrollSabado23 = document.getElementById("scrollSabado23");
+
+function izSab23() {
+  scrollSabado23.scrollBy({left: -300, behavior: "smooth"});
+
+}
+function derSab23() {
+  scrollSabado23.scrollBy({left: 300, behavior: "smooth"});
+}
+
+
+// Domingo 24
+var scrollDomingo24 = document.getElementById("scrollDomingo24");
+
+function izDom24() {
+  scrollDomingo24.scrollBy({left: -300, behavior: "smooth"});
+
+}
+function derDom24() {
+  scrollDomingo24.scrollBy({left: 300, behavior: "smooth"});
+}
+
+
+// Lunes 25
+var scrollLunes25 = document.getElementById("scrollLunes25");
+
+function izLun25() {
+  scrollLunes25.scrollBy({left: -300, behavior: "smooth"});
+
+}
+function derLun25() {
+  scrollLunes25.scrollBy({left: 300, behavior: "smooth"});
+}
+
+
+// Martes 26
+var scrollMartes26 = document.getElementById("scrollMartes26");
+
+function izMar26() {
+  scrollMartes26.scrollBy({left: -300, behavior: "smooth"});
+
+}
+function derMar26() {
+  scrollMartes26.scrollBy({left: 300, behavior: "smooth"});
+}
+
+
+// Miércoles 27
+var scrollMiercoles27 = document.getElementById("scrollMiercoles27");
+
+function izMie27() {
+  scrollMiercoles27.scrollBy({left: -300, behavior: "smooth"});
+
+}
+function derMie27() {
+  scrollMiercoles27.scrollBy({left: 300, behavior: "smooth"});
+}
+
+
+// Jueves 28
+var scrollJueves28 = document.getElementById("scrollJueves28");
+
+function izJue28() {
+  scrollJueves28.scrollBy({left: -300, behavior: "smooth"});
+
+}
+function derJue28() {
+  scrollJueves28.scrollBy({left: 300, behavior: "smooth"});
+}
+
+
+// Viernes 29
+var scrollViernes29 = document.getElementById("scrollViernes29");
+
+function izVie29() {
+  scrollViernes29.scrollBy({left: -300, behavior: "smooth"});
+
+}
+function derVie29() {
+  scrollViernes29.scrollBy({left: 300, behavior: "smooth"});
+}
